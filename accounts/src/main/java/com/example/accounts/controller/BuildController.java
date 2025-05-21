bb63b55c1f58164b81f88dc3367136fdc8fbb4b3
Sameep Mondhe <sameepmondhe@gmail.com>
2025-05-20 17:43:50 -0700

    config-changes

--- accounts/src/main/java/com/example/accounts/controller/BuildController.java
+++ accounts/src/main/java/com/example/accounts/controller/BuildController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BuildController {
 
-    @Value("${build.version}")
+    @Value("${build.version:unknown}")
     private String buildVersion;
 
     @GetMapping("/version")
